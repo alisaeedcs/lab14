0c78bd893d24f52786172c26a9002cacca608403
Ali Saeed <asaeed5637@gmail.com>
2024-09-24 15:20:17 -0700

    Final commit

--- main.cpp
+++ main.cpp
@@ -22,5 +22,32 @@ public:
 };
 
 int main() {
+	//declare colors
+	Color teal, orange, turquoise;
+
+	//teal color
+	teal.setRed(0);
+	teal.setGreen(128);
+	teal.setBlue(128);
+
+	//orange color
+	orange.setRed(255);
+	orange.setGreen(165);
+	orange.setBlue(0);
+
+	//turquoise color
+	turquoise.setRed(48);
+	turquoise.setGreen(213);
+	turquoise.setBlue(200);
+
+	//output
+	cout << "Teal" << endl;
+	teal.print();
+
+	cout << "Orange" << endl;
+	orange.print();
+
+	cout << "Turquoise" << endl;
+	turquoise.print();
 	return 0;
 }
